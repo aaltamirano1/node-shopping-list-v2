4a77cf4bd73d19df30585590530ab0b746b97acc
Angel Altamirano <angel.g.altam@gmail.com>
2019-01-17 23:46:02 -0500

    create a POST endpoint for recipes endpoint

--- server.js
+++ server.js
@@ -47,10 +47,22 @@ app.post('/shopping-list', jsonParser, (req, res) => {
   res.status(201).json(item);
 });
 
-
 app.get('/recipes', (req, res) => {
   res.json(Recipes.get());
 })
+
+app.post('/recipes', jsonParser, (req, res)=>{
+  const requiredFields = ['name', 'ingredients'];
+  requiredFields.forEach(field=>{
+    if (!(field in req.body)) {
+      const msg = `Missing \`${field}\` in request body`;
+      console.error(msg);
+      return res.status(400).send(msg);
+    }
+  });
+  const item = Recipes.create(req.body.name, req.body.ingredients);
+  res.status(201).json(item);
+});
 
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
